56cc4539a8dd89a39b21e3c74ee095e173fe6cfc
Yalord96 <sergeynosenko96@gmail.com>
2020-06-08 15:59:25 +0300

    watcher

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var pipeline = require('readable-stream').pipeline;
+// var watch = require('gulp-watch');
+ 
+// gulp.task('js', function () {
+//   return pipeline(
+//         gulp.src('js/app.js'),
+//         uglify(),
+//         gulp.dest('dist/js')
+//   );
+// });
+
+gulp.task('scss', function () {
+  return gulp.src('src/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function(){
+  // gulp.watch( 'src/*.scss', gulp.series(['scss']));
+  gulp.watch( 'src/**/*.scss', gulp.series(['scss']));
+  // gulp.watch( 'src/**.js', gulp.series(['js']))
+});
